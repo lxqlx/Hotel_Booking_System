02cd953dda95eabc2fff2b7fb20022ff3f6edae9
Li Xiangqun <lxqlxfighting@gmail.com>
2014-10-29 05:16:07 +0800

    add .htaccess file

--- remote_sync_to.sh
+++ remote_sync_to.sh
@@ -1,3 +1,4 @@
 # !/env/bash
 #cp -r ./* /var/www/html/
 scp -r ./*.* a0091794@cs2102:~/public_html/
+scp -r ./.htaccess a0091794@cs2102:~/public_html/
--- server_sync_to.sh
+++ server_sync_to.sh
@@ -1,3 +1,4 @@
 # !/env/bash
 cp -r ./*.* /var/www/html/
+cp -r ./.htaccess /var/www/html/
 #scp -r ./* a0091794@cs2102:~/public_html/
